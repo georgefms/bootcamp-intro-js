90b372531bd79f1bc055c10cd72ab6ddcce62241
George Felipe de Magalhães Silva <georgef@ufpa.br>
2023-04-28 13:42:25 -0300

    Feat: working with fetchAPI

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('Sucesso!');
+
+const offset = 0;
+const limit = 12;
+const URL = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
+
+fetch(URL)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
