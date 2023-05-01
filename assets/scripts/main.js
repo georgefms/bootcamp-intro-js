701ed769da54e704afa9e78e0b5938c43e169aa4
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-05-01 19:04:52 -0300

    Refactor: dividing responsibilities

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,24 @@
+const pokemonList = document.getElementById('pokemonList');
 
-const offset = 0;
-const limit = 12;
-const URL = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
-
-fetch(URL)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
+function createPokemonHtml(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="pokemon-number">#001</span>
+        <span class="pokemon-name">${pokemon.name}</span>
+        <div class="pokemon-details">
+            <ol class="pokemon-types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+pokeApi.getPokemonList()
+.then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += createPokemonHtml(pokemon);
+    }
+})
