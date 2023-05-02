9912f24c38ff5f10febbf9e4f4dd50fa5eb5b9d4
George Felipe de Magalhães Silva <georgef@ufpa.br>
2023-05-02 14:31:38 -0300

    Style: changing type colors

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,12 +3,12 @@ const pokemonList = document.getElementById('pokemonList');
 
 function createPokemonHtml(pokemon) {
     return `
-    <li class="pokemon ${pokemon.type}}">
+    <li class="${pokemon.type} pokemon">
         <span class="pokemon-number">#${pokemon.number}</span>
         <span class="pokemon-name">${pokemon.name}</span>
         <div class="pokemon-details">
             <ol class="pokemon-types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
             </ol>
             <img src="${pokemon.img}" alt="${pokemon.name}">
         </div>
